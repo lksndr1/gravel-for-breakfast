8d597ac17cbc099bcb3be1647837c80e78cbabb8
hettfield git <olekpl90@gmail.com>
2025-05-29 18:30:04 +0200

    svg burger-button

--- header.php
+++ header.php
@@ -23,32 +23,44 @@
 
                 <div class='header-buttons'>
                     <?php
-                    $burger_menu = get_field('burger_menu', 'option');
-                    if ($burger_menu) :
-                        $burger_menu_url = $burger_menu['url'];
-                        $burger_menu_alt = $burger_menu['alt'];
+                        $svg_url = get_field('burger_menu_icon', 'option');
+                        if ($svg_url) {
+                            $cache_key = 'cached_svg_' . md5($svg_url);
+                            $svg_content = get_transient($cache_key);
+
+                            if (!$svg_content) {
+                                $svg_path = ABSPATH . str_replace(home_url('/'), '', $svg_url);
+                                if (file_exists($svg_path)) {
+                                    $svg_content = file_get_contents($svg_path);
+                                    set_transient($cache_key, $svg_content, 12 * HOUR_IN_SECONDS);
+                                }
+                            }
+
+                            if ($svg_content) {
+                                echo '<button id="header-dropdown-menu-toggle" class="burger-button">';
+                                echo $svg_content;
+                                echo '</button>';
+                            }
+                        }
                     ?>
-                        <button id='header-dropdown-menu-toggle'>
-                            <img src="<?php echo esc_url($burger_menu_url); ?>" alt="<?php echo esc_attr($burger_menu_alt); ?>">
+
+                    <div class='header-dropdown-menu-wrapper'>
+                        <button id='close-dropdown-menu' type="button" aria-label="Close menu">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5.8335 5.83331L14.1668 14.1666M5.8335 14.1666L14.1668 5.83331" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
                         </button>
-                        <div class='header-dropdown-menu-wrapper'>
-                            <button id='close-dropdown-menu' type="button" aria-label="Close menu">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M5.8335 5.83331L14.1668 14.1666M5.8335 14.1666L14.1668 5.83331" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </button>
-                            <nav class="dropdown-menu-nav">
-                                <?php wp_nav_menu([
-                                    'theme_location'       => 'header',
-                                    'container'            => false,
-                                    'menu_class'           => 'dropdown-menu-list',
-                                    'menu_id'              => false,
-                                    'echo'                 => true,
-                                    'items_wrap'           => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>',
-                                ]); ?>
-                            </nav>
-                        </div>
-                     <?php endif; ?>
+                        <nav class="dropdown-menu-nav">
+                            <?php wp_nav_menu([
+                                'theme_location'       => 'header',
+                                'container'            => false,
+                                'menu_class'           => 'dropdown-menu-list',
+                                'menu_id'              => false,
+                                'echo'                 => true,
+                                'items_wrap'           => '<ul id="%1$s" class="header_list %2$s">%3$s</ul>',
+                            ]); ?>
+                        </nav>
+                    </div>
 
                     <!-- button link -->
                     <?php
