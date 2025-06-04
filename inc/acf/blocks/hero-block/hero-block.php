64f0eee8cead850384779ca54d6480f3e2f20f13
hettfield git <olekpl90@gmail.com>
2025-06-04 12:22:53 +0200

    hero image

--- inc/acf/blocks/hero-block/hero-block.php
+++ inc/acf/blocks/hero-block/hero-block.php
@@ -6,7 +6,10 @@ $default_classes = [
     'left-col' => 'left-col',
     'right-col' => 'right-col',
     'right-col-wrapper' => 'right-col-wrapper',
+    'description' => 'description',
     'email-copyright-wrapper' => 'email-copyright-wrapper',
+    'hero-image-wrapper' => 'hero-image-wrapper',
+    
 ];
 
 $modules_file = get_template_directory() . '/assets/blocks/styles/modules.json';
@@ -27,7 +30,7 @@ if (file_exists($modules_file)) {
                 </div>
                 <div class="<?php echo esc_attr($classes['right-col']); ?>">
                     <div class="<?php echo esc_attr($classes['right-col-wrapper']); ?>">
-                        <p><?php echo get_field('description') ?></p>
+                        <p class="<?php echo esc_attr($classes['description']); ?>"><?php echo get_field('description') ?></p>
                         <?php
                             $hero_link = get_field('hero_link');
                             if (!empty($hero_link)) :
@@ -75,5 +78,14 @@ if (file_exists($modules_file)) {
             </div>
 
         </div>
+
+        <?php
+        $hero_image = get_field('hero_image');
+        if(!empty($hero_image)) : ?>
+            <div class="<?php echo esc_attr($classes['hero-image-wrapper']); ?>">
+                <img src="<?php echo esc_url($hero_image['url']) ?>" alt="<?php echo esc_attr($hero_image['alt'])?>">
+            </div>
+        <?php endif; ?>
+
     </section>
 </div>
